399ebd5618b01e99074c64344902209c08e4fb94
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2021-03-19 15:23:18 +0100

    Do not include pid in lock file for compiler

--- aesara/compile/compilelock.py
+++ aesara/compile/compilelock.py
@@ -34,14 +34,12 @@ def force_unlock(lock_dir: os.PathLike):
     lock_dir : os.PathLike
         Path to a directory that was locked with `lock_ctx`.
     """
-
-    fl = filelock.FileLock(os.path.join(lock_dir, ".lock"))
+    lockfile = os.path.join(lock_dir, 'lock')
+    fl = filelock.FileLock(lockfile)
     fl.release(force=True)
 
-    dir_key = f"{lock_dir}-{os.getpid()}"
-
-    if dir_key in local_mem._locks:
-        del local_mem._locks[dir_key]
+    if lockfile in local_mem._locks:
+        del local_mem._locks[lockfile]
 
 
 @contextmanager
@@ -61,22 +59,15 @@ def lock_ctx(lock_dir: os.PathLike = None, *, timeout: typing.Optional[float] = 
         lock_dir = config.compiledir
     if timeout == -1:
         timeout = config.compile__timeout
-    elif not (timeout is None or timeout > 0):
-        raise ValueError(f"Timeout parameter must be None or positive. Got {timeout}.")
 
     # locks are kept in a dictionary to account for changing compiledirs
-    dir_key = f"{lock_dir}-{os.getpid()}"
+    lockfile = os.path.join(lock_dir, 'lock')
 
-    if dir_key not in local_mem._locks:
-        local_mem._locks[dir_key] = True
-        fl = filelock.FileLock(os.path.join(lock_dir, ".lock"))
-        fl.acquire(timeout=timeout)
-        try:
-            yield
-        finally:
-            if fl.is_locked:
-                fl.release()
-            if dir_key in local_mem._locks:
-                del local_mem._locks[dir_key]
+    if lockfile in local_mem._locks:
+        lock = local_mem._locks[lockfile]
     else:
+        lock = filelock.FileLock(lockfile, timeout=timeout)
+        local_mem._locks[lockfile] = lock
+
+    with lock.acquire(timeout=timeout):
         yield
